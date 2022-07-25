1847dee27975c631ecf66b796a65c0c0deff478e
SDKSSH <sdk_ssh@protonmail.com>
2022-07-25 02:30:09 +0200

    Add lombok

--- src/main/java/me/sdkssh/shodron/ShodronMod.java
+++ src/main/java/me/sdkssh/shodron/ShodronMod.java
@@ -1,12 +1,16 @@
 package me.sdkssh.shodron;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("shodron")
-public class ShodronMod
-{
+public class ShodronMod {
+    public static final String MOD_ID = "shodron";
     public ShodronMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 }
